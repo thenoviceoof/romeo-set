493bfc041ab58a4d674461de6ba2f625d5f0a030
Nathan Hwang <thenoviceoof@gmail.com>
2012-03-27 06:17:08 -0400

    wrote some thoughts in sram

--- sram.vhd
+++ sram.vhd
@@ -1,118 +1,52 @@
---Legal Notice: (C)2007 Altera Corporation. All rights reserved.  Your
---use of Altera Corporation's design tools, logic functions and other
---software and tools, and its AMPP partner logic functions, and any
---output files any of the foregoing (including device programming or
---simulation files), and any associated documentation or information are
---expressly subject to the terms and conditions of the Altera Program
---License Subscription Agreement or other applicable license agreement,
---including, without limitation, that your use is for the sole purpose
---of programming logic devices manufactured by Altera and sold by Altera
---or its authorized distributors.  Please refer to the applicable
---agreement for further details.
-
-
--- turn off superfluous VHDL processor warnings 
--- altera message_level Level1 
--- altera message_off 10034 10035 10036 10037 10230 10240 10030 
-
-library altera;
-use altera.altera_europa_support_lib.all;
-
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
-entity sram is 
-        port (
-              -- inputs:
-                 signal address : IN STD_LOGIC_VECTOR (17 DOWNTO 0);
-                 signal byteenable : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-                 signal chipselect : IN STD_LOGIC;
-                 signal read : IN STD_LOGIC;
-                 signal write : IN STD_LOGIC;
-                 signal writedata : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+entity sram is
+	port(
+		sram_data : inout std_logic_vector(15 downto 0);
+		sram_addr : out std_logic_vector(17 downto 0);
+		sram_ub_n,
+		sram_lb_n,
+		sram_we_n,
+		sram_ce_n,
+		sram_oe_n : out std_logic;
+		
+		x : in std_logic_vector(9 downto 0); -- 640<1024 (10b)
+		y : in std_logic_vector(8 downto 0); -- 480<512  (9b)
+		rv : out std_logic_vector(7 downto 0);
+		wv : in std_logic_vector(7 downto 0);
+		-- !!! we need two ports, but we have one. how do we overcome this?
+		-- !!! some sort of time-multiplexing? (ie. shift register)
+		-- !!! concievably, the input queue could never let up
+		-- !!! so how could we draw without dropping?
+		-- !!! maybe we have something like 8 write-waits, and drop after that?
+		re : in std_logic -- control whether to read data
+		we : in std_logic -- control whether to write data
+	);
+end sram;
 
-              -- outputs:
-                 signal SRAM_ADDR : OUT STD_LOGIC_VECTOR (17 DOWNTO 0);
-                 signal SRAM_CE_N : OUT STD_LOGIC;
-                 signal SRAM_DQ : INOUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-                 signal SRAM_LB_N : OUT STD_LOGIC;
-                 signal SRAM_OE_N : OUT STD_LOGIC;
-                 signal SRAM_UB_N : OUT STD_LOGIC;
-                 signal SRAM_WE_N : OUT STD_LOGIC;
-                 signal readdata : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
-              );
-end entity sram;
-
-
-architecture europa of sram is
-component de2_sram_controller is 
-           port (
-                 -- inputs:
-                    signal address : IN STD_LOGIC_VECTOR (17 DOWNTO 0);
-                    signal byteenable : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-                    signal chipselect : IN STD_LOGIC;
-                    signal read : IN STD_LOGIC;
-                    signal write : IN STD_LOGIC;
-                    signal writedata : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-
-                 -- outputs:
-                    signal SRAM_ADDR : OUT STD_LOGIC_VECTOR (17 DOWNTO 0);
-                    signal SRAM_CE_N : OUT STD_LOGIC;
-                    signal SRAM_DQ : INOUT STD_LOGIC_VECTOR (15 DOWNTO 0);
-                    signal SRAM_LB_N : OUT STD_LOGIC;
-                    signal SRAM_OE_N : OUT STD_LOGIC;
-                    signal SRAM_UB_N : OUT STD_LOGIC;
-                    signal SRAM_WE_N : OUT STD_LOGIC;
-                    signal readdata : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
-                 );
-end component de2_sram_controller;
-
-                signal internal_SRAM_ADDR :  STD_LOGIC_VECTOR (17 DOWNTO 0);
-                signal internal_SRAM_CE_N :  STD_LOGIC;
-                signal internal_SRAM_LB_N :  STD_LOGIC;
-                signal internal_SRAM_OE_N :  STD_LOGIC;
-                signal internal_SRAM_UB_N :  STD_LOGIC;
-                signal internal_SRAM_WE_N :  STD_LOGIC;
-                signal internal_readdata :  STD_LOGIC_VECTOR (15 DOWNTO 0);
-
+architecture sram_arch of sram is
+	signal addr : std_logic_vector(17 downto 0);
+	signal upper : std_logic;
+	signal lower : std_logic;
 begin
-
-  --the_de2_sram_controller, which is an e_instance
-  the_de2_sram_controller : de2_sram_controller
-    port map(
-      SRAM_ADDR => internal_SRAM_ADDR,
-      SRAM_CE_N => internal_SRAM_CE_N,
-      SRAM_DQ => SRAM_DQ,
-      SRAM_LB_N => internal_SRAM_LB_N,
-      SRAM_OE_N => internal_SRAM_OE_N,
-      SRAM_UB_N => internal_SRAM_UB_N,
-      SRAM_WE_N => internal_SRAM_WE_N,
-      readdata => internal_readdata,
-      address => address,
-      byteenable => byteenable,
-      chipselect => chipselect,
-      read => read,
-      write => write,
-      writedata => writedata
-    );
-
-
-  --vhdl renameroo for output signals
-  SRAM_ADDR <= internal_SRAM_ADDR;
-  --vhdl renameroo for output signals
-  SRAM_CE_N <= internal_SRAM_CE_N;
-  --vhdl renameroo for output signals
-  SRAM_LB_N <= internal_SRAM_LB_N;
-  --vhdl renameroo for output signals
-  SRAM_OE_N <= internal_SRAM_OE_N;
-  --vhdl renameroo for output signals
-  SRAM_UB_N <= internal_SRAM_UB_N;
-  --vhdl renameroo for output signals
-  SRAM_WE_N <= internal_SRAM_WE_N;
-  --vhdl renameroo for output signals
-  readdata <= internal_readdata;
-
-end europa;
-
+	-- generate the address
+	addr <= x(9 downto 0) & y(8 downto 1);
+	upper <= y(0);
+	
+	-- sram outputs
+	sram_addr <= addr;
+	sram_ub_n <= NOT upper;
+	sram_lb_n <= upper;
+	sram_we_n <= NOT we;
+	sram_ce_n <= '0'; -- always enable the chip
+	sram_oe_n <= we;
+	sram_data <= wv & "00000000" when (upper='1' AND we='1') else
+				 "00000000" & wv when (upper='0' AND we='1') else
+				 (others => 'Z'); -- we='0', reading
+	-- module outputs
+	rv <= sram_data(15 downto 8) when (upper='1' AND we='0') else
+	      sram_data(7 downto 0) when (upper='0' AND we='0') else
+		  "00000000";
+end sram_arch;
