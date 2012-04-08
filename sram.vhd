fbc35a049f40de5e35da966c0655c73fde2fbb01
Nathan Hwang <thenoviceoof@gmail.com>
2012-04-07 23:08:11 -0400

    packing up changes before nuking my local repo

--- sram.vhd
+++ sram.vhd
@@ -4,124 +4,156 @@ use ieee.numeric_std.all;
 
 -- this module expects to bridge from 50Mhz writes to 25MHz reads
 entity sram is
-  port(
-    -- clocks
-    clk_50 : std_logic;
-    clk_25 : std_logic;
-
-    -- SRAM_DQ, 16 bit data
-    sram_data : inout std_logic_vector(15 downto 0);
-    -- SRAM_ADDR, 18 bit address space (256k)
-    sram_addr : out std_logic_vector(17 downto 0);
-    -- SRAM_UB_N, *LB_N, upper and lower byte masks
-    sram_ub_n,
-    sram_lb_n,
-    -- SRAM_WE_N, write enable
-    sram_we_n,
-    -- SRAM_CE_N, chip enable (power up chip)
-    sram_ce_n,
-    -- SRAM_OE_N, output enable (when reading)
-    sram_oe_n : out std_logic;
-
-    -- 640<1024 (10 bits)
-    rx : in std_logic_vector(9 downto 0);
-    -- 480<512  (9 bits)
-    ry : in std_logic_vector(8 downto 0);
-    -- same, but for writing
-    wx : in std_logic_vector(9 downto 0);
-    wy : in std_logic_vector(8 downto 0);
-    -- read/write values (8 bits)
-    rv : out std_logic_vector(7 downto 0);
-    wv : in std_logic_vector(7 downto 0);
-    -- read/write controls
-    -- reading takes precedence
-    re : in std_logic;
-    we : in std_logic
-    );
+	port(
+		rhold_out : out std_logic_vector(15 downto 0);
+		re_count_out : out std_logic_vector(1 downto 0);
+		
+		-- clocks
+		clk_50 : in std_logic;
+		clk_25 : in std_logic;
+	
+		-- SRAM_DQ, 16 bit data
+		sram_data : inout std_logic_vector(15 downto 0);
+		-- SRAM_ADDR, 18 bit address space (256k)
+		sram_addr : out std_logic_vector(17 downto 0);
+		-- SRAM_UB_N, *LB_N, upper and lower byte masks
+		sram_ub_n,
+		sram_lb_n,
+		-- SRAM_WE_N, write enable
+		sram_we_n,
+		-- SRAM_CE_N, chip enable (power up chip)
+		sram_ce_n,
+		-- SRAM_OE_N, output enable (when reading)
+		sram_oe_n : out std_logic;
+	
+		-- 640<1024 (10 bits)
+		rx : in std_logic_vector(9 downto 0);
+		-- 480<512  (9 bits)
+		ry : in std_logic_vector(8 downto 0);
+		-- same, but for writing
+		wx : in std_logic_vector(9 downto 0);
+		wy : in std_logic_vector(8 downto 0);
+		-- read/write values (8 bits)
+		rv : out std_logic_vector(7 downto 0);
+		wv : in std_logic_vector(7 downto 0);
+		-- read/write controls
+		-- reading takes precedence
+		re : in std_logic;
+		we : in std_logic
+	);
 end sram;
 
 architecture sram_arch of sram is
-  signal addr : std_logic_vector(17 downto 0);
-  -- temp addr signals
-  signal raddr : std_logic_vector(17 downto 0);
-  signal waddr : std_logic_vector(17 downto 0);
+	signal addr : std_logic_vector(17 downto 0);
+	-- temp addr signals
+	signal raddr : std_logic_vector(17 downto 0);
+	signal waddr : std_logic_vector(17 downto 0);
+	
+	signal last : std_logic;
 
-  -- byte mask, due to 16bit words in SRAM
-  signal mask : std_logic_vector(1 downto 0);
+	-- byte mask, due to 16bit words in SRAM
+	signal mask : std_logic_vector(1 downto 0);
 
-  -- handle reading from the SRAM
-  signal re_count : unsigned(1 downto 0);
-  signal rre : std_logic;
-  -- a register for the stored bits
-  signal rhold : std_logic_vector(15 downto 0);
+	-- handle reading from the SRAM
+	signal re_count : unsigned(1 downto 0) := "00";
+	signal rre : std_logic;
+	-- a register for the stored bits
+	signal rhold : std_logic_vector(15 downto 0);
 
-  -- buffer for writes, length 2
-  signal we_buffer : std_logic_vector(1 downto 0);
-  signal wv_buffer : std_logic_vector(15 downto 0);
-  signal waddr_buffer : std_logic_vector(37 downto 0); -- 19 each
-  -- outputs from the buffer, r for really
-  signal rwe : std_logic;
-  signal rwv : std_logic_vector(7 downto 0);
-  signal rwaddr : std_logic_vector(18 downto 0);
+	-- buffer for writes, length 1
+	signal we_buffer : std_logic;
+	signal wv_buffer : std_logic_vector(7 downto 0);
+	signal waddr_buffer : std_logic_vector(18 downto 0); -- 19 each
+	-- outputs from the buffer, r for really
+	signal rwe : std_logic;
+	signal rwv : std_logic_vector(7 downto 0);
+	signal rwaddr : std_logic_vector(18 downto 0);
 begin
-  -- determine whether we really need to read from the SRAM
-  rre <= re when re_count="00" else '0';
-  -- and fetch the rest of the results from the write buffer
-  rwe <= we_buffer(0) and (not rre); -- + check rre
-  rwv <= wv_buffer(7 downto 0);
-  rwaddr <= waddr_buffer(18 downto 0);
+	-- TEST SHIT
+	rhold_out <= rhold;
+	re_count_out <= std_logic_vector(re_count);
+	
+	-- determine whether we really need to read from the SRAM
+	rre <= re when re_count="00" else '0';
+	-- and fetch the rest of the results from the write buffer
+	rwe <= we_buffer and (not rre); -- + check rre
+	rwv <= wv_buffer(7 downto 0);
+	rwaddr <= waddr_buffer(18 downto 0);
 
-  -- generate the address
-  raddr <= ry(8 downto 0) & rx(9 downto 1);
-  -- for the waddr
-  waddr <= wy(8 downto 0) & wx(9 downto 0)
-  rwaddr <= waddr_buffer(18 downto 1);
-  addr <= raddr when rre='1' else rwaddr;
-  -- get the last bit
-  last <= rx(0) when rre='1' else rwaddr(0);
+	-- generate the address
+	raddr <= ry(8 downto 0) & rx(9 downto 1);
+	-- for the waddr
+	waddr <= wy(8 downto 0) & wx(9 downto 0);
+	rwaddr <= waddr_buffer(18 downto 1);
+	addr <= raddr when rre='1' else rwaddr;
+	-- get the last bit
+	last <= rx(0) when rre='1' else rwaddr(0);
 
-  -- find out if we need to mask either byte
-  mask <= "11" when rre else -- always read both bytes
-          "01" when last='0' else 
-          "10"; -- last='1'
-  
-  -- sram outputs
-  sram_addr <= addr;
-  -- going to have to redo this part, migth have to use both bytes
-  sram_ub_n <= not mask(1);
-  sram_lb_n <= not mask(0);
-  -- only enable write enable when, well, writing
-  sram_we_n <= not rwe;
-  -- only enable the output when reading
-  sram_oe_n <= not rre;
-  -- always power up the chip
-  sram_ce_n <= '0';
+	-- find out if we need to mask either byte
+	mask <= "11" when rre='1' else -- always read both bytes
+			"01" when last='0' else 
+			"10"; -- last='1'
 
-  -- try to generate the right data
-  sram_data <= rwv & "00000000" when (last='1' and rwe='1') else
-               "00000000" & rwv when (last='0' and rwe='1') else
-               (others => 'Z'); -- rwe='0', reading
-  -- module outputs
-  rv <= sram_data(7 downto 0) when rre='1' else
-        rhold(7 downto 0) when (re='1' and last='0') else
-        rhold(15 downto 8) when (re='1' and last='1') else
-        "00000000";
+	-- sram outputs
+	sram_addr <= addr;
+	-- going to have to redo this part, migth have to use both bytes
+	sram_ub_n <= not mask(1);
+	sram_lb_n <= not mask(0);
+	-- only enable write enable when, well, writing
+	sram_we_n <= not rwe;
+	-- only enable the output when reading
+	sram_oe_n <= not rre;
+	-- always power up the chip
+	sram_ce_n <= '0';
 
-  -- handle synchronous things
-  process(clk_50)
-  begin
-    -- increment the re_count, keeps track of when to really read
-    re_count <= re_count+1 when re='1' else '0';
+	-- try to generate the right data
+	sram_data <= rwv & "00000000" when (last='1' and rwe='1') else
+				"00000000" & rwv when (last='0' and rwe='1') else
+				(others => 'Z'); -- rwe='0', reading
+	-- module outputs
+	rv <= sram_data(7 downto 0) when rre='1' else
+		rhold(7 downto 0) when (re='1' and last='0') else
+		rhold(15 downto 8) when (re='1' and last='1') else
+		"00000000";
 
-    -- save the results of the read
-    rhold <= sram_data when rre='1' else (others => '0');
+	-- !!! test, link writes directly
+	we_buffer <= we;
+	wv_buffer <= wv;
+	waddr_buffer <= waddr;
 
-    -- buffer for writes
-    we_buffer(1) <= we; -- always write the current we
-    we_buffer(0) <= we_buffer(1) when (rre='0') else we_buffer(0);
-    wv_buffer(15 downto 8) <= wv;
-    wv_buffer(7 downto 0) <= wv_buffer(1) when (rre='0') else wv_buffer(0);
-    waddr_buffer(37 downto 19) <= waddr_b; -- always write the current we
-    waddr_buffer(18 downto 0) <= waddr_buffer(37 downto 19) when (rre='0') else waddr_buffer(18 downto 0);
-  end process;
+	-- handle synchronous things
+	process(clk_50)
+	begin
+		if rising_edge(clk_50) then
+			-- increment the re_count, keeps track of when to really read
+			if re='1' then
+				re_count <= re_count + "01";
+			else
+				re_count <= (others => '0');
+			end if;
+
+			-- save the results of the read
+			if rre='1' then
+				rhold <= sram_data;
+			else
+				rhold <= rhold;
+			end if;
+
+
+	-- buffer for writes
+--	we_buffer(1) <= we; -- always write the current we
+--	wv_buffer(15 downto 8) <= wv;
+--	waddr_buffer(37 downto 19) <= waddr; -- always write the current we
+--	if rre='0' then
+--		we_buffer(0) <= we_buffer(1);
+--		wv_buffer(7 downto 0) <= wv_buffer(15 downto 8);
+--		waddr_buffer(18 downto 0) <= waddr_buffer(37 downto 19);
+--	else
+--		we_buffer(0) <= we_buffer(0);
+--		wv_buffer(7 downto 0) <= wv_buffer(7 downto 0);
+--		waddr_buffer(18 downto 0) <= waddr_buffer(18 downto 0);
+--	end if;
+
+		end if;
+	end process;
 end sram_arch;
--- sram_tb.vhd
+++ sram_tb.vhd
@@ -6,95 +6,204 @@ entity sram_tb is
 end sram_tb;
 
 architecture tb of sram_tb is
-  signal clk : std_logic := '0';
+	signal clk_50 : std_logic := '1';
+	signal clk_25 : std_logic := '1';
+	
+	signal sram_data : std_logic_vector(15 downto 0) := "ZZZZZZZZZZZZZZZZ";
+	signal sram_addr : std_logic_vector(17 downto 0) := "ZZZZZZZZZZZZZZZZZZ";
+	signal sram_ub_n : std_logic := 'Z';
+	signal sram_lb_n : std_logic := 'Z';
+	signal sram_we_n : std_logic := 'Z';
+	signal sram_ce_n : std_logic := 'Z';
+	signal sram_oe_n : std_logic := 'Z';
+	
+	signal r_e : std_logic := '0';
+	signal r_x : std_logic_vector(9 downto 0) := "XXXXXXXXXX";
+	signal r_y : std_logic_vector(8 downto 0) :=  "XXXXXXXXX";
+	signal read_sram : std_logic_vector(7 downto 0) := "XXXXXXXX";
+	
+	signal w_e : std_logic := '0';
+	signal w_x : std_logic_vector(9 downto 0) := "XXXXXXXXXX";
+	signal w_y : std_logic_vector(8 downto 0) :=  "XXXXXXXXX";
+	signal write_sram : std_logic_vector(7 downto 0) := "XXXXXXXX";
 
-  signal sram_data : std_logic_vector(15 downto 0);
-  signal sram_addr : std_logic_vector(17 downto 0);
-  signal sram_ub_n : std_logic;
-  signal sram_lb_n : std_logic;
-  signal sram_we_n : std_logic;
-  signal sram_ce_n : std_logic;
-  signal sram_oe_n : std_logic;
+	signal rhold : std_logic_vector(15 downto 0) := "ZZZZZZZZZZZZZZZZ";
+	signal re_count : std_logic_vector(1 downto 0) := "ZZ";
 
-  signal r_e : std_logic := '0';
-  signal r_x : std_logic_vector(9 downto 0) := "XXXXXXXXXX";
-  signal r_y : std_logic_vector(8 downto 0) :=  "XXXXXXXXX";
-  signal read_sram : std_logic_vector(7 downto 0) := "XXXXXXXX";
-
-  signal w_e : std_logic := '0';
-  signal w_x : std_logic_vector(9 downto 0) := "XXXXXXXXXX";
-  signal w_y : std_logic_vector(8 downto 0) :=  "XXXXXXXXX";
-  signal write_sram : std_logic_vector(7 downto 0) := "XXXXXXXX";
+--	component sram is
+--	port(
+--		rhold_out : out std_logic_vector(15 downto 0);
+--		re_count_out : out std_logic_vector(1 downto 0);
+--	
+--		-- clocks
+--		clk_50 : in std_logic;
+--		clk_25 : in std_logic;
+--
+--		-- SRAM_DQ, 16 bit data
+--		sram_data : inout std_logic_vector(15 downto 0);
+--		-- SRAM_ADDR, 18 bit address space (256k)
+--		sram_addr : out std_logic_vector(17 downto 0);
+--		-- SRAM_UB_N, *LB_N, upper and lower byte masks
+--		sram_ub_n,
+--		sram_lb_n,
+--		-- SRAM_WE_N, write enable
+--		sram_we_n,
+--		-- SRAM_CE_N, chip enable (power up chip)
+--		sram_ce_n,
+--		-- SRAM_OE_N, output enable (when reading)
+--		sram_oe_n : out std_logic;
+--
+--		-- 640<1024 (10 bits)
+--		rx : in std_logic_vector(9 downto 0);
+--		-- 480<512  (9 bits)
+--		ry : in std_logic_vector(8 downto 0);
+--		-- same, but for writing
+--		wx : in std_logic_vector(9 downto 0);
+--		wy : in std_logic_vector(8 downto 0);
+--		-- read/write values (8 bits)
+--		rv : out std_logic_vector(7 downto 0);
+--		wv : in std_logic_vector(7 downto 0);
+--		-- read/write controls
+--		-- reading takes precedence
+--		re : in std_logic;
+--		we : in std_logic
+--	);
+--	end component;
 begin
-  -- instantiate unit
-  sram_u : entity work.sram
-  port map ( 
-    sram_data => sram_data,
-    sram_addr => sram_addr,
-    sram_ub_n => sram_ub_n,
-    sram_lb_n => sram_ub_n,
-    sram_we_n => sram_we_n,
-    sram_ce_n => sram_ce_n,
-    sram_oe_n => sram_oe_n,
 
-    rx => r_x,
-    ry => r_y,
-    rv => read_sram,
-    wx => w_x,
-    wy => w_y,
-    wv => write_sram,
-    re => r_e,
-    we => w_e
-  );
+	clk_50 <= not clk_50 after 10 ns; -- 50MHz
+	clk_25 <= not clk_25 after 20 ns; -- 25MHz
 
-  --clk <= not clk after 10 ns; -- 50MHz
-  clk <= not clk after 20 ns; -- 25MHz
+	process
+	begin
+		-- wait a 25mhz cycle
+		wait for 39 ns;
 
-  process
-  begin
---    wait for 10 ns;
+		-- 1 --------------------
+		-- do a write
+		w_e <= '1';
+		w_x <= "0000000100";
+		w_y <= "000000010";
+		write_sram <= "10101010";
+		-- clean up after the write
+		wait for 20 ns;
+		w_e <= '0';
+		w_x <= "XXXXXXXXXX";
+		w_y <= "XXXXXXXXX";
+		write_sram <= "XXXXXXXX";
 
-    -- try writing
---    wait for 0 ns;
---    w_e <= '1';
---    w_x <= "0000000100";
---    w_y <= "000000010";
---    write_sram <= "10101010";
---    wait for 20 ns;
---    w_e <= '0';
---    w_x <= "XXXXXXXXXX";
---    w_y <= "XXXXXXXXX";
---    write_sram <= "XXXXXXXX";
---    wait for 20 ns;
+		-- wait a cycle
+		wait for 20 ns;
 
-    -- try reading
---    r_e <= '1';
---    r_x <= "0000000100";
---    r_y <= "000000010";
---    wait for 12 ns;
---    sram_data <= "0000000010101010";
---    wait for 8 ns;
---    r_e <= '0';
---    r_x <= "XXXXXXXXXX";
---    r_y <= "XXXXXXXXX";
---    wait for 2 ns;
---    sram_data <= "0000000000000000";
+		-- 2 --------------------
+		-- do a read
+		r_e <= '1';
+		r_x <= "0000000100";
+		r_y <= "000000010";
+		wait for 12 ns;
+		sram_data <= "1010000010101010";
+		wait for 8 ns;
+		-- and interleave a write
+		w_e <= '1';
+		w_x <= "0000110100";
+		w_y <= "011100010";
+		write_sram <= "11100010";
+		wait for 2 ns;
+		-- clean up the read from sram
+		sram_data <= "ZZZZZZZZZZZZZZZZ";
+		wait for 18 ns;
+		-- and clean up both
+		r_e <= '0';
+		r_x <= "XXXXXXXXXX";
+		r_y <= "XXXXXXXXX";
+		-- clean write
+		w_e <= '0';
+		w_x <= "XXXXXXXXXX";
+		w_y <= "XXXXXXXXX";
+		write_sram <= "XXXXXXXX";
 
-    -- reading with 25MHz clock
-    wait for 20 ns;
-    r_e <= '1';
-    r_x <= "0000000100";
-    r_y <= "000000010";
-    wait for 12 ns;
-    sram_data <= "0000000010101010";
-    wait for 28 ns;
-    r_e <= '0';
-    r_x <= "XXXXXXXXXX";
-    r_y <= "XXXXXXXXX";
-    wait for 2 ns;
-    sram_data <= "0000000000000000";
+		-- 3 --------------------
+		-- keep reading, and do a write at the same time
+		r_e <= '1';
+		r_x <= "0000000101";
+		r_y <= "000000010";
+		-- write somewhere completely random
+		w_e <= '1';
+		w_x <= "0001000000";
+		w_y <= "000001010";
+		write_sram <= "11101111";
+		wait for 20 ns;
+		-- clean write
+		w_e <= '0';
+		w_x <= "XXXXXXXXXX";
+		w_y <= "XXXXXXXXX";
+		write_sram <= "XXXXXXXX";
+		wait for 20 ns;
+		-- clean read
+		r_e <= '0';
+		r_x <= "XXXXXXXXXX";
+		r_y <= "XXXXXXXXX";	
 
-    -- end
-    wait;
-  end process;
+		-- 4 --------------------
+		-- keep reading, do writes to both
+		r_e <= '1';
+		r_x <= "0000000110";
+		r_y <= "000000010";
+		-- write
+		w_e <= '1';
+		w_x <= "1001000110";
+		w_y <= "001011010";
+		write_sram <= "10101011";
+		wait for 12 ns;
+		sram_data <= "1010011011101110";
+		wait for 8 ns;
+		-- and now do another write
+		w_e <= '1';
+		w_x <= "1001001111";
+		w_y <= "001011110";
+		write_sram <= "11111111";
+		-- clear the read
+		wait for 2 ns;
+		sram_data <= "ZZZZZZZZZZZZZZZZ";
+		wait for 18 ns;
+		-- clean up both
+		r_e <= '0';
+		r_x <= "XXXXXXXXXX";
+		r_y <= "XXXXXXXXX";
+		-- clean write
+		w_e <= '0';
+		w_x <= "XXXXXXXXXX";
+		w_y <= "XXXXXXXXX";
+		write_sram <= "XXXXXXXX";
+
+		-- end
+		wait;
+	end process;
+
+	-- instantiate unit
+	uut : entity work.sram
+	port map (
+		rhold_out => rhold,
+		re_count_out => re_count,
+		
+		clk_50 => clk_50,
+		clk_25 => clk_25,
+    
+		sram_data => sram_data,
+		sram_addr => sram_addr,
+		sram_ub_n => sram_ub_n,
+		sram_lb_n => sram_ub_n,
+		sram_we_n => sram_we_n,
+		sram_ce_n => sram_ce_n,
+		sram_oe_n => sram_oe_n,
+
+		rx => r_x,
+		ry => r_y,
+		rv => read_sram,
+		wx => w_x,
+		wy => w_y,
+		wv => write_sram,
+		re => r_e,
+		we => w_e
+	);
 end tb;
