97c667078ff55360e7c412f4ce7d8e9d5d8003dc
Nathan Hwang <beta.entity.k@gmail.com>
2012-04-10 00:27:09 -0400

    Updated test_image.py to not hog memory

--- ifm_test_scripts/test_image.py
+++ ifm_test_scripts/test_image.py
@@ -4,30 +4,53 @@
 import sys
 import numpy as np
 from PIL import Image
+import pickle
 
+# use steve's stuff
 import test_ifm_utils as ifm
 
 def main(path):
     # read the file
     f = open(path)
-    lines = f.readlines()[1:] # throw away first line
-    data = [l.split(" ") for l in lines]
-    # convert the location data
-    data = [(ifm.from_fixed(int(d[1], 2)),
-             ifm.from_fixed(int(d[2], 2)),
-             int(d[3])) for d in data]
-    # find the range of the data
-    #arange = (min([d[0] for d in data]), max([d[0] for d in data]))
-    #brange = (min([d[1] for d in data]), max([d[1] for d in data]))
-    arange = (-2.0, 2.0)
-    brange = (-1.0, 1.0)
+    line = f.readline() # throw away first line
     # init the image
     img = np.zeros((480,640), np.uint8)
-    # write out the data
-    for d in data:
-        x = round((d[0]+arange[0])/(arange[1]-arange[0])*640)%640
-        y = round((d[1]+brange[0])/(brange[1]-brange[0])*480)%480
-        img[y][x] = 2*d[2]
+    # find the range of the data
+    print "Finding the range of the data"
+    arange_min = 0
+    arange_max = 0
+    brange_min = 0
+    brange_max = 0
+    i = 0
+    data = {}
+    for line in f:
+        elems = line.split(" ")
+        d = (ifm.from_fixed(int(elems[1], 2)),
+             ifm.from_fixed(int(elems[2], 2)))
+        arange_min = min(arange_min, d[0])
+        arange_max = max(arange_min, d[0])
+        brange_min = min(arange_min, d[1])
+        brange_max = max(arange_min, d[1])
+        # writing once
+        data[d] = int(elems[3])
+        # count
+        i += 1
+        if i % 100000 == 0:
+            print i
+    arange = (arange_min, arange_max)
+    brange = (brange_min, brange_max)
+    print arange
+    print brange
+    # arange = (-2.0, 2.0)
+    # brange = (-1.0, 1.0)
+
+    # go through, line by line
+    print "Filling out the image"
+    for coord, count in data.iteritems():
+        # write out the data
+        x = round((coord[0]+arange[0])/(arange[1]-arange[0])*640)%640
+        y = round((coord[1]+brange[0])/(brange[1]-brange[0])*480)%480
+        img[y][x] = 2*count
     # write out the file
     im = Image.fromarray(img, mode="L")
     im.save("ifm_test.png")
