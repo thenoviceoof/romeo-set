bf7346318278abfb4d3b8cb618f569f9cf848d8d
Nathan Hwang <thenoviceoof@gmail.com>
2012-04-20 11:09:38 -0400

    edited pll_tb to ensure behavior

--- src/pll_tb.vhd
+++ src/pll_tb.vhd
@@ -2,15 +2,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity plltb is
-end plltb;
+entity pll_tb is
+end pll_tb;
 
-architecture tb of plltb is
-signal CLOCK_50				: std_logic;
+architecture tb of pll_tb is
+signal CLOCK_50				: std_logic := '0';
 signal clk_50				: std_logic;
 signal clk_25				: std_logic;
 signal clk_sram				: std_logic;
-signal pll_locked				: std_logic;
 
 component pll5025 is
   
@@ -18,8 +17,7 @@ component pll5025 is
 		inclk0		: IN STD_LOGIC;
 		c0		: OUT STD_LOGIC ;
 		c1		: OUT STD_LOGIC ;
-		c2		: OUT STD_LOGIC ;
-		locked		: OUT STD_LOGIC 
+		c2		: OUT STD_LOGIC
 	);
 end component;
 
@@ -37,6 +35,5 @@ begin
 		c0		=> clk_50,
 		c1		=> clk_25,
 		c2		=> clk_sram
-		locked		=> pll_locked
 	);
 end tb;
