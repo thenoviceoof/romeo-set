c3fabc9fc92906db8c8ec2c958a104a912c65a11
Luis E. P <l31g@hotmail.com>
2012-05-07 22:12:41 -0400

    Sorry Kevin for the gitignore

---
+++ src/software/hello_world_1_syslib/Debug/obj/alt_exception_entry.d
@@ -0,0 +1,3 @@
+obj/alt_exception_entry.o:  \
+ /opt/altera/altera7.2/ip/nios2_ip/altera_nios2/HAL/src/alt_exception_entry.S \
+  system_description/system.h
---
+++ src/software/pr1_syslib/Debug/obj/alt_environ.d
@@ -0,0 +1,3 @@
+obj/alt_environ.o:  \
+ /opt/altera/altera7.2/nios2eds/components/altera_hal/HAL/src/alt_environ.c \
+  /opt/altera/altera7.2/nios2eds/components/altera_hal/HAL/inc/os/alt_syscall.h
