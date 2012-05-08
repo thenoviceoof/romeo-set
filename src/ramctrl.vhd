2b2f36a68662b9202323e616f65c48562fbb4ddd
Nathan Hwang <thenoviceoof@gmail.com>
2012-05-08 02:09:54 -0400

    updated ramctrl, still unsure of design

--- src/ramctrl.vhd
+++ src/ramctrl.vhd
@@ -6,38 +6,29 @@ entity ramctrl is
 	port(
 	clk						: in std_logic;
 	reset_n					: in std_logic;
-	read					: in std_logic;
 	write					: in std_logic;
 	chipselect				: in std_logic;
-	address					: in unsigned(1 downto 0);
-	readdata				: out unsigned(1 downto 0);
-	writedata				: out unsigned(1 downto 0);
+	address					: in unsigned(0 downto 0);
+	writedata				: in unsigned(7 downto 0);
 
-	addressout				: in unsigned(1 downto 0);
-	readaddr				: out unsigned(1 downto 0);
+	read_data				: out unsigned(1 downto 0)
 	);
-end ramcon;
+end ramctrl;
 
 architecture ramarch of ramctrl is
 
-	type ram_type is array(1 downto 0) of unsigned(0 downto 0);
-	
-	signal RAM				: ram_type;
-	signal read_addr		: integer;
+	-- do we need to write to the bits seperately?
+	signal reg : unsigned(1 downto 0);
+
 	begin
 
 	process(clk)
 	begin
 		if rising_edge(clk) then
-			read_addr	<= to_integer(addressout);
-			readaddr	<= addressout;
-			readdata	<= RAM(to_integer(addressout));
-			if reset_n = '0' then
-				read_addr	<= 0;
-			else
-				if chipselect = '1' and write = '1' then
-					RAM(to_integer(address)) <= writedata(17 downto 0);
-				end if;
+			read_data	<= reg;
+			
+			if chipselect = '1' and write = '1' then
+				reg <= writedata(1 downto 0);
 			end if;
 		end if;
 	end process;
