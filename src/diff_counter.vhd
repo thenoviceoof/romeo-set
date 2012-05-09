b283d453181427a15cdaab875b85c372cf3e6e71
Stephen D Pratt <sdp2128@micro8.ilab.columbia.edu>
2012-05-09 04:49:37 -0400

    Documenting: diff counter

--- src/diff_counter.vhd
+++ src/diff_counter.vhd
@@ -1,3 +1,12 @@
+---------------------------------------------------------------------
+--diff_counter.vhd
+--
+--A device that increments a value by some differential, adjusting 
+--the sum as necessary to assure convergence to a maximum value.
+--
+--Author: Stephen Pratt
+---------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -51,26 +60,36 @@ begin
 		c_next <= c_curr+1;
 		v_next <= v_curr + v_diff;
 		
-		itr_next <= itr_count+1;
+		itr_next <= itr_count+1; --itr_next is leap counter
+		
+		--make adjustment on leap count
 		if leap = '1' then 
 			v_next <= v_curr + v_diff + 1;
-			itr_next <= (others=>'0');
+			itr_next <= (others=>'0'); --reset leap counter to zero
 		end if;
 
 		leap <= '0';
+		
+		--if we complete a leap interval, we should leap next cycle
 		if itr_next = v_leap then
 			leap <= '1';
 		end if;	
 
 		itr_count <= itr_count;		
+		
+		--Reset operation - transition to start state
 		if reset = '1' then
 			v_curr <= v_min;
 			c_curr <= (others=>'0');
 			itr_count <= (others=>'0');
 			at_max <= '0';
 			ready_sig <= '1';
+			
+		--Maximum iteration reached - ransition to max state
 		elsif c_curr = max_itr then
 			at_max <= '1';
+			
+		--Next value requested
 		elsif next_val = '1' then			
 			c_curr <= c_next;
 			v_curr <= v_next;			
